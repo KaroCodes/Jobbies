5e9c826920717a6defed0dc4900e8664178519d5
Karolina Pawlikowska <eikatte@gmail.com>
2016-08-04 06:10:01 -0700

    Skeleton of add job activity and models for job description.

---
+++ Jobbies/app/src/main/java/com/androidcamp/jobbies/AddNewJobFragment.java
@@ -0,0 +1,25 @@
+package com.androidcamp.jobbies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Karolina Pawlikowska on 8/4/16.
+ */
+public class AddNewJobFragment extends Fragment {
+
+    private JobDescription jobDescription;
+
+    public AddNewJobFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_new_job, container, false);
+    }
+
+}
